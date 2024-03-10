81a283e5d925acb28ce1cbada5b7d665ccad280a
Imamul Islam nAyeem <imamul.nayeem@hotmail.com>
2024-03-10 18:06:11 +0600

    Add project files.

---
+++ Product.API/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Product.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductsController : ControllerBase
+{
+    [HttpGet]
+    public IEnumerable<Product> Get()
+    {
+        IEnumerable<Product> productList = new List<Product>
+        {
+            new Product{Id = 1, Name="Somusa"  },
+            new Product{Id = 2, Name="Singara"  },
+        };
+
+        return productList;
+    }
+}
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+}
---
+++ Shop.API/Controllers/ShopsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shop.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ShopsController : ControllerBase
+{
+    // GET: api/<ShopsController>
+    [HttpGet]
+    public IEnumerable<Shop> Get()
+    {
+        return new List<Shop>
+        {
+            new Shop{ Id = 1, ShopName= "Mina bazar", Location= "sector 11, Uttara"},
+            new Shop{ Id = 2, ShopName= "CP", Location= "Ashkona, Dakshinkhan"},
+            new Shop{ Id = 3, ShopName= "Bata", Location= "Bot-tola"},
+        };
+    }
+
+    // GET api/<ShopsController>/5
+    //[HttpGet("{id}")]
+    //public string Get(int id)
+    //{
+    //    return "value";
+    //}
+
+    //// POST api/<ShopsController>
+    //[HttpPost]
+    //public void Post([FromBody] string value)
+    //{
+    //}
+
+}
+
+public class Shop
+{
+    public int Id { get; set; }
+    public string ShopName { get; set; } = string.Empty;
+    public string Location { get; set; } = string.Empty;
+}
